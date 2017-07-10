7a315a1f68240be7c2de5ab946c39582f2e08063
ayan-majumder <ayan.majumder@innoraft.com>
2017-07-10 18:27:15 +0530

    index file, loginn design, profile design html created

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,20 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic','ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "login.html"
+    })
+    .when("/profile", {
+        templateUrl : "profile.html"
+    });
+});
+// angular.module('app',['ngRoute']);
+// var appl= angular.module('combined',['app','starter']);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard

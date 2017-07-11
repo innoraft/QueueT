b1c18d404d54cc884b4e0b2f1532bdc0ce67ed83
Tridib Ghosh <tridib.ghosh@innoraft.com>
2017-07-11 17:48:57 +0530

    basic google authentication done,printed authenticated users name in console

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,17 +1,27 @@
 // Todo Controller
 app.controller('AppCtrl',function($scope,$firebaseObject){
 
-	var ref = firebase.database().ref();
-	$scope.name = $firebaseObject(ref);
+  var provider = new firebase.auth.GoogleAuthProvider();
 
 
-	var abc = $scope.name;
-	console.log(abc.first);
-
-	setTimeout(function(){ console.log(abc.first); }, 3000);
-
-	// $scope.saveTodo =  function(){
-	// 	console.log($scope.name);
-	// };
+	$scope.googleAuth =  function(){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user.displayName);
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+	};
 
 });

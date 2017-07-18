8857ac0a1d030e38412fcf396eb9e7762b88e5c4
tridib1002 <tridibghoshce@gmail.com>
2017-07-18 10:55:08 +0530

    google authentication without redirection - Done

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,55 +1,34 @@
 // Todo Controller
 app.controller('AppCtrl',function($scope,$firebaseObject){
-
   var provider = new firebase.auth.GoogleAuthProvider();
-
   // provider.addScope('https://www.googleapis.com/auth/plus.login');
 
+
 	$scope.googleAuth =  function(){
-
-
-    // // firebase authentication with Redirection method.
-    // firebase.auth().getRedirectResult().then(function(result) {
-    //
-    // if (result.credential) {
-    //   // This gives you a Google Access Token. You can use it to access the Google API.
-    //   var token = result.credential.accessToken;
-    // }
-    // // The signed-in user info.
-    // var user = result.user;
-    //
-    // console.log(user.displayName);
-    //
-    // }).catch(function(error) {
-    //   // Handle Errors here.
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    //   // The email of the user's account used.
-    //   var email = error.email;
-    //   // The firebase.auth.AuthCredential type that was used.
-    //   var credential = error.credential;
-    //   // ...
-    // });
-
-
     // firebase authentication with popup window.
+		// $scope.name="abc";
     firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log(user.displayName);
-    // ...
+    	var token = result.credential.accessToken;
+    	// The signed-in user info.
+    	var user = result.user;
+			// $scope.name = "hello";
+			$scope.$apply(function(){
+				$scope.name=user.displayName;
+			});
+			$scope.name = user.displayName;
+    	console.log(user.displayName);
+    	// ...
     }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-    // console.log("entering error");
+    	// Handle Errors here.
+    	var errorCode = error.code;
+    	var errorMessage = error.message;
+    	// The email of the user's account used.
+    	var email = error.email;
+    	// The firebase.auth.AuthCredential type that was used.
+    	var credential = error.credential;
+    	// ...
+    	// console.log("entering error");
     });
 
 	};
